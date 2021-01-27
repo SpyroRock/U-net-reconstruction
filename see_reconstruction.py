9ea383fdc189957014aaf8ed66e8994ca47e1a06
Spiridoula <smenounou@gmail.com>
2021-01-27 16:48:07 +0200

    reconstruction

--- see_reconstruction.py
+++ see_reconstruction.py
@@ -3,14 +3,19 @@ from keras.models import load_model
 import matplotlib.pyplot as plt
 from numpy import load
 import pickle
+import tensorflow as tf
 
 #pickle_in = open('reconstruction.pkl', 'rb')
 #reconstructor = pickle.load(pickle_in)
 
 features_symbol = load('features_data.npy')
+print(tf.constant(features_symbol))
 print(features_symbol.shape)
 features_symbol_predicted = load('features_predicted.npy')
+print(tf.constant(features_symbol_predicted))
 print(features_symbol_predicted.shape)
+
+
 
 #print(features_symbol[10, :, :, 0])
 
